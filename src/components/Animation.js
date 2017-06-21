2f25d4988a5cb2d9b54447453d60f012d84ef9db
Dan <hi@danmchugh.co.uk>
2017-06-22 00:36:28 +0100

    Added basic animations for chat messages.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { TimelineMax, Sine } from 'gsap'
-import { randomInt } from '../helpers'
 import Waypoint from 'react-waypoint'
+import { randomInt } from '../helpers'
 
 function appear(id) {
   let time = randomInt(1,5)
@@ -20,11 +20,13 @@ function appear(id) {
 class Animation extends Component {
 
   componentWillMount() {
+    // Setup object id, and a place to store animations
     this.id = this.props.id+'-anim'
     this.anims = {}
   }
 
   componentDidMount() {
+    // Setup animations once the component is actually in scope
     this.anims.enter = appear(this.id)
   }
 
@@ -33,13 +35,13 @@ class Animation extends Component {
   }
 
   waypointLeave = () => {
-    this.anims.enter.stop()
+    this.anims.enter.pause(0, true) // resets animation timeline
   }
 
   render() {
     return (
       <div className="Animation">
-        <Waypoint onEnter={this.waypointEnter} onLeave={this.waypointLeave} >
+        <Waypoint onEnter={this.waypointEnter} onLeave={this.waypointLeave}>
           <div id={this.id}>
             Animation here
           </div>
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react'
+import { TimelineMax, Sine, Back } from 'gsap'
+import Waypoint from 'react-waypoint'
+
+function appear(id) {
+  // let elements = ["#"+id+"-0", "#"+id+"-1", "#"+id+"-2"]
+  let elements = document.getElementById(id).getElementsByClassName('message')
+  return new TimelineMax({ paused: true })
+  .staggerFrom(elements,0.3,{
+    css: {
+      transform: 'translateX(-20px)',
+      opacity: 0
+    },
+    delay: 1,
+    ease: Back.easeOut
+  }, 1)
+}
 
 class Chat extends Component {
 
+  componentWillMount() {
+    // Setup object id, and a place to store animations
+    this.id = this.props.id+'-chat'
+    this.anims = {}
+  }
+
+  componentDidMount() {
+    // Setup animations once the component is actually in scope
+    this.anims.enter = appear(this.id)
+  }
+
+  waypointEnter = () => {
+    this.anims.enter.play()
+  }
+
+  waypointLeave = () => {
+    this.anims.enter.pause(0, true) // resets animation timeline
+  }
+
+
   renderMessage = (message, i) => (
-    <div key={i} className="message chat-message"> <span>{ message }</span> </div>
+    <div key={i} id={`${this.id}-${i}`} className="message chat-message"> <span>{ message }</span> </div>
   )
 
   render() {
     const { messages } = this.props
     return (
-      <div className="Chat">
-        { messages.map((m, i) => this.renderMessage(m, i)) }
-      </div>
+      <Waypoint onEnter={this.waypointEnter} onLeave={this.waypointLeave}>
+        <div id={this.id} className="Chat">
+          { messages.map((m, i) => this.renderMessage(m, i)) }
+        </div>
+      </Waypoint>
     )
   }
 }
