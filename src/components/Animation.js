189953d6979d345708aa2398f3b6112dbafe6a50
Dan <hi@danmchugh.co.uk>
2017-06-21 23:43:24 +0100

    Added helpers.js file.  Created randomised dummy animations.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { TweenMax, Sine } from 'gsap'
 import * as ScrollMagic from 'scrollmagic'
+import { randomInt } from '../helpers'
 // import '../vendor/debug.addIndicators'
 // import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
 // import 'imports?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
@@ -13,15 +14,18 @@ class Animation extends Component {
     this.id = this.props.id+'-anim'
   }
   componentDidMount() {
-    var tween = TweenMax.to("#"+this.id,2,{
-      scale: 3,
-      // rotation: 180,
+    let time = randomInt(1,5)
+    let scale = randomInt(1,4)
+    let rotation = randomInt(0,180)
+    let tween = TweenMax.to("#"+this.id,time,{
+      scale: scale,
+      rotation: rotation,
       repeat: -1,
       yoyo: true,
       ease: Sine.easeInOut
     })
 
-    var scene = new ScrollMagic
+    let scene = new ScrollMagic
     .Scene({triggerElement: "#"+this.props.id, duration: 600})
     .setTween(tween)
     // .addIndicators()
---
+++ src/helpers.js
@@ -0,0 +1,5 @@
+// Export any functions in here to be used as a helper/operator/appliable function
+
+export function randomInt(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
