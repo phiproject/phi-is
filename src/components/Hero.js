7f219a840942d467892051d2fba8e0dc0bf3ffce
Dan <hi@danmchugh.co.uk>
2017-06-23 15:25:37 +0100

    Added buttons to './components/Hero.js'

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
 
 class Hero extends Component {
-  render() {
-    const { data } = this.props
-    return (
-      <div className="Hero">
 
-        <div>
-          <h1 className="Xrellax" data-rellax-speed="1">{data.title}</h1>
-          <h2 className="Xrellax" data-rellax-speed="3">{data.subtitle}</h2>
-        </div>
+   renderButton = (button, i) => {
+      return (
+         <a key={i} href={button.url} alt={button.alt}>
+            <button className={button.type}>
+               <span>{button.subtitle}</span>
+               {button.title}
+            </button>
+         </a>
+      )
+   }
 
-      </div>
-    )
-  }
+   render() {
+      const { data } = this.props
+      return (
+         <div className="Hero">
+
+            <div>
+               <h1 className="Xrellax" data-rellax-speed="1">{data.title}</h1>
+               <h2 className="Xrellax" data-rellax-speed="3">{data.subtitle}</h2>
+               <div>
+                  {data.buttons.map((button,i)=>this.renderButton(button,i))}
+               </div>
+            </div>
+
+         </div>
+      )
+   }
 }
 
 export default Hero
